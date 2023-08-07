08a44480932f689b62aab6ade5d7fb1789b4320d
nuaa520 <141616559+nuaa520@users.noreply.github.com>
2023-08-07 22:47:16 +0800

    Update BlogPost.js

--- themes/nobelium/components/BlogPost.js
+++ themes/nobelium/components/BlogPost.js
@@ -8,12 +8,13 @@ const BlogPost = ({ post }) => {
 
       <article key={post.id} className="mb-6 md:mb-8">
         <header className="flex flex-col justify-between md:flex-row md:items-baseline">
+  <time className="flex-shrink-0 text-gray-600 dark:text-gray-400">
+            {formatDate(post?.publishDay || post.createdTime, BLOG.LANG)}
+          </time>
           <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
             {post.title}
           </h2>
-          <time className="flex-shrink-0 text-gray-600 dark:text-gray-400">
-            {formatDate(post?.publishDay || post.createdTime, BLOG.LANG)}
-          </time>
+          
         </header>
         <main>
           <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
